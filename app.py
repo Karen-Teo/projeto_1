669ab2bad86777ba69c5426e3fef1c07b9f20859
Káren <141977786+Karen-Teo@users.noreply.github.com>
2023-11-18 11:36:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,44 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-     
-df_carros = pd.read_csv('C:\\Users\\Veronica\\Downloads\\vehicles.csv') # lendo os dados
-hist_button = st.button('Criar histograma') # criar um botão
-     
-if hist_button: # se o botão for clicado
-    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
-    fig = px.histogram(df_carros, x="odometer")
-    st.plotly_chart(fig, use_container_width=True)
 
-scatter_button = st.button('Criar gráfico de dispersão') # criar outro botão
+# Carregando os dados
+df_carros = pd.read_csv('C:\\Users\\Veronica\\Downloads\\vehicles.csv')
 
-if scatter_button: # se o botão do gráfico de dispersão for clicado
-    st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
-    fig = px.scatter(df_carros, x="odometer", y="price")
-    st.plotly_chart(fig, use_container_width=True)
+# Cabeçalho da aplicação
+st.title('Análise de Anúncios de Vendas de Carros')
+
+# Texto introdutório
+st.header('Explorando Dados de Anúncios de Carros')
+st.write('Este aplicativo permite explorar visualmente os dados de anúncios de vendas de carros.')
+
+# Pergunta 1: Criar um histograma da quilometragem dos carros
+st.header('Histograma da Quilometragem')
+hist_button = st.button('Criar Histograma')
+
+if hist_button:
+    st.write('Visualizando a distribuição da quilometragem dos carros')
+    fig_hist = px.histogram(df_carros, x='odometer')
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+# Pergunta 2: Criar um gráfico de dispersão entre preço e ano de fabricação
+st.header('Gráfico de Dispersão: Preço vs. Ano de Fabricação')
+scatter_button = st.button('Criar Gráfico de Dispersão')
+
+if scatter_button:
+    st.write('Visualizando a relação entre preço e ano de fabricação dos carros')
+    fig_scatter = px.scatter(df_carros, x='year', y='price', color='manufacturer')
+    st.plotly_chart(fig_scatter, use_container_width=True)
+
+# Caixa de seleção para visualizar carros de uma marca específica
+st.header('Visualizar Carros por Marca')
+selected_manufacturer = st.selectbox('Selecione uma Marca de Carro',
+                                     df_carros['manufacturer'].unique())
+
+if selected_manufacturer:
+    st.write(f'Visualizando carros da marca {selected_manufacturer}')
+    filtered_data = df_carros[df_carros['manufacturer'] == selected_manufacturer]
+    fig_manufacturer = px.histogram(filtered_data, x='odometer', color='model')
+    st.plotly_chart(fig_manufacturer, use_container_width=True)
+
+     
